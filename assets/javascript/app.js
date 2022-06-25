fb8a31cefc8739a112a0e3846ab597880f5c1a20
Flipper <filipe@motionfirestudios.com.au>
2022-06-25 18:47:31 +0800

    utilised Mays CSS to adjust JS and HTML files, added missing files ie imgs, css files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,13 @@
-const userInput = document.getElementById('user-input');
-const output = document.getElementById('recipes');
-const submitForm = document.getElementById('submit');
+$(document).foundation();
 
-// spoonacular
-submitForm.addEventListener('submit', function(event){
+const userInput = document.getElementById('user-input');
+const recipeCreationEl = document.getElementById('recipes');
+const submitFormEl = document.getElementById('form-index');
+const pastSearchesEl = document.getElementById('past-results');
+
+submitFormEl.addEventListener('submit', function(event){
 
     event.preventDefault();
-    output.textContent = '';
 
     const search = userInput.value;
     const searchPlus = search.replaceAll(' ','+')
@@ -15,7 +16,14 @@ submitForm.addEventListener('submit', function(event){
         return
     };
 
+    if (!pastResults.includes(search)){
+        pastResults.push(search);
+        localStorage.setItem('history', JSON.stringify(pastResults));
+    };
+
     userInput.value = '';
+
+    generateSearchHistory();
 
     ingredientSearch(searchPlus);
 })
@@ -31,8 +39,12 @@ function ingredientSearch(searchPlus){
     })
 }
 
+// change 0 to i
 function recipeInformation(foodData){
-    for (let i = 0; i < 4; i++) {
+
+    recipeCreationEl.textContent = '';
+    
+    for (let i = 0; i < 2; i++) {
         fetch(`https://api.spoonacular.com/recipes/${foodData[i].id}/information?apiKey=196192673e7b41b2b8a7e8f82c389470&includeNutrition=false`)
         .then(function(response){
             return response.json();
@@ -45,35 +57,89 @@ function recipeInformation(foodData){
 
 function generateRecipes(foodData){
 
+    const section = document.createElement('section');
+    section.setAttribute('class','recipe-card')
+    const article = document.createElement('article');
+    article.setAttribute('class','recipe-info')
+
     const h1 = document.createElement('h1');
-    const img = document.createElement('img');
-    
-    h1.textContent = foodData.title;
-    img.setAttribute('src',foodData.image);
-    img.setAttribute('style','width: 200px;');
-    output.appendChild(h1);
-    output.appendChild(img);
-    
-    const p = document.createElement('p');
-    p.textContent = foodData.instructions;
-    output.appendChild(p);
-
-    for (let i = 0; i < 20; i++) {
-        const ingredient = foodData.extendedIngredients[i].name
-        const measurement = foodData.extendedIngredients[i].measures.metric.amount+' '+foodData.extendedIngredients[i].measures.metric.unitShort
-
-        if (ingredient === ''){
-            return
-        }
-        
-        const li = document.createElement('li');
-        li.textContent = measurement + ' ' + ingredient;
-        output.appendChild(li)
-
+    if (foodData.vegan){
+        h1.textContent = 'V | ' + foodData.title;
+    } else if (foodData.glutenFree){
+        h1.textContent = 'GF | ' + foodData.title;
+    } else {
+        h1.textContent = foodData.title;
     }
 
+    article.appendChild(h1);
+    
+    const img = document.createElement('img');
+    img.setAttribute('src',foodData.image);
+    img.setAttribute('class','recipe-img');
+    
+    const p = document.createElement('p');
+    let instructions = foodData.instructions;
+    if (instructions !== null){
+        const removeFromString = ['<li>','</li>','<ol>','</ol>']
+        for (let i = 0; i < removeFromString.length; i++) {
+            const removed = removeFromString[i];
+            const recipeInstructions = instructions.replaceAll(removed,'')
+            instructions = recipeInstructions
+        } 
+
+        p.textContent = 'Serves: ' + foodData.servings + ' | ' + 'Ready-in: ' + foodData.readyInMinutes + ' minutes | Instructions: ' + instructions;
+        article.appendChild(p);
+        
+        const ol = document.createElement('ol');
+        ol.setAttribute('class','recipe-ingredients')
+        const howManyIngredients = foodData.extendedIngredients
+        for (let i = 0; i < howManyIngredients.length; i++) {
+            const ingredient = foodData.extendedIngredients[i].name;
+            const measurement = foodData.extendedIngredients[i].measures.metric.amount+' '+foodData.extendedIngredients[i].measures.metric.unitShort;
+
+            if (ingredient === ''){
+                return;
+            }
+
+            const li = document.createElement('li');
+            li.textContent = measurement + ' ' + ingredient;
+            ol.appendChild(li)
+        }
+
+        article.appendChild(ol)
+        section.appendChild(img);
+        section.appendChild(article);
+        recipeCreationEl.appendChild(section);
+    } else {
+
+        const h2 = document.createElement('h2');
+        h2.textContent = 'Try a broader search!';
+        recipeCreationEl.appendChild(h2);
+    }
 }
 
-    
+let pastResults = [];
+function generateSearchHistory(){
+    pastSearchesEl.textContent = '';
+
+    let storedHistory = JSON.parse(localStorage.getItem("history"));
+    if (storedHistory !== null){
+        pastResults = storedHistory;
+    }
+
+    for (let i = 0; i < pastResults.length; i++) {
+        const pastResult = pastResults[i];
+        const button = document.createElement('button');
+        button.setAttribute('class','button search-history');
+        button.textContent = pastResult;
+
+        button.addEventListener('click', function(){
+            const searchPlus = (button.textContent).replaceAll(' ','+')
+            ingredientSearch(searchPlus)
+        })
+        
+        pastSearchesEl.appendChild(button);
+    }
+}
 
     
