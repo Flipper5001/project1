751507d69deffd2cbd60568099a736a821008698
Flipper <filipe@motionfirestudios.com.au>
2022-06-27 21:56:09 +0800

    updates to html and css to match JS, created movie api and elements, general team design changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,30 +4,82 @@ const userInput = document.getElementById('user-input');
 const recipeCreationEl = document.getElementById('recipes');
 const submitFormEl = document.getElementById('form-index');
 const pastSearchesEl = document.getElementById('past-results');
+const lazyBtn = document.getElementById('lazy-btn');
+const moviesEl = document.getElementById('movies');
+const moviesHeader = document.getElementById('movies-header');
+
+function getRandomMovies(){
+
+    moviesHeader.classList.remove('none');
+    moviesEl.textContent= ''
+
+    const movielist = ['food','cloudy_with_a_chance_of_meatballs','ratatouille','sausage_party','the_founder','sideways','chef','foodfight','chocolat','willy_wonka_&_the_chocolate_factory','attack_of_the_killer_tomatoes','good_burger']
+    const movies = movielist.sort(() => 0.5 - Math.random());
+    
+    for (let i = 0; i < 4; i++) {
+        const randomMovie = movies[i];
+
+        fetch(`https://www.omdbapi.com/?apikey=1dd69296&t=${randomMovie}`)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(movie){
+            return generateMovie(movie);
+        })
+    }
+}
+
+function generateMovie(movie) {
+    const img = document.createElement('img');
+    img.setAttribute('src',movie.Poster)
+    img.setAttribute('style','width:300px; height:450px; cursor:pointer;')
+    img.onclick = function(){
+        window.location.href = `https://www.imdb.com/title/${movie.imdbID}`, '_blank';
+    }
+    moviesEl.appendChild(img);
+}
 
 submitFormEl.addEventListener('submit', function(event){
-
+    
     event.preventDefault();
-
+    
     const search = userInput.value;
     const searchPlus = search.replaceAll(' ','+')
-
+    
     if (searchPlus === ''){
         return
     };
-
+    
     if (!pastResults.includes(search)){
-        pastResults.push(search);
+        pastResults.unshift(search);
         localStorage.setItem('history', JSON.stringify(pastResults));
     };
-
+    
     userInput.value = '';
-
+    
     generateSearchHistory();
-
-    ingredientSearch(searchPlus);
+    
+    ingredientSearch(searchPlus)
+    // issue: after generating random recipe, then searching for one, the random recipes do no disappear and remain
+    getRandomMovies();
+    
 })
 
+lazyBtn.addEventListener('click', function(event){
+    
+    event.preventDefault();
+
+    fetch('https://api.spoonacular.com/recipes/random?apiKey=196192673e7b41b2b8a7e8f82c389470&number=4&instructionsRequired=true&addRecipeInformation=true')
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(foodData){
+        return recipeInformation(foodData.recipes);
+    })
+    .then(function(){
+        getRandomMovies();
+    })
+})
 
 function ingredientSearch(searchPlus){
     fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=196192673e7b41b2b8a7e8f82c389470&ingredients=${searchPlus}&number=4&instructionsRequired=true&addRecipeInformation=true`)
@@ -35,16 +87,22 @@ function ingredientSearch(searchPlus){
         return response.json();
     })
     .then(function(foodData){
-        return recipeInformation(foodData);
+        if (foodData.length === 0){
+            const h2 = document.createElement('h2');
+            h2.setAttribute('class','text-center sub-font alert')
+            h2.textContent = 'No food found, but we did find these movies of food :)'
+            return recipeCreationEl.appendChild(h2)
+        } else {
+            return recipeInformation(foodData);
+        }
     })
 }
 
-// change 0 to i
 function recipeInformation(foodData){
 
     recipeCreationEl.textContent = '';
     
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < 4; i++) {
         fetch(`https://api.spoonacular.com/recipes/${foodData[i].id}/information?apiKey=196192673e7b41b2b8a7e8f82c389470&includeNutrition=false`)
         .then(function(response){
             return response.json();
@@ -110,12 +168,7 @@ function generateRecipes(foodData){
         section.appendChild(img);
         section.appendChild(article);
         recipeCreationEl.appendChild(section);
-    } else {
-
-        const h2 = document.createElement('h2');
-        h2.textContent = 'Try a broader search!';
-        recipeCreationEl.appendChild(h2);
-    }
+    } 
 }
 
 let pastResults = [];
@@ -127,6 +180,10 @@ function generateSearchHistory(){
         pastResults = storedHistory;
     }
 
+    if (pastResults.length > 4){
+        pastResults = pastResults.slice(0,5);
+    }
+    
     for (let i = 0; i < pastResults.length; i++) {
         const pastResult = pastResults[i];
         const button = document.createElement('button');
@@ -142,4 +199,4 @@ function generateSearchHistory(){
     }
 }
 
-    
+generateSearchHistory();    
